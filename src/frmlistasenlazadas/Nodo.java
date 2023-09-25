7fb81119aa35c1d576c2198ef0443922860488e6
LAB-USR-LN6377-A0607 <LAB-USR-LN6377-A0607@UTP362029.grupoutp.net>
2023-09-25 10:24:51 -0500

    :)

---
+++ src/frmlistasenlazadas/Nodo.java
@@ -0,0 +1,39 @@
+
+package frmlistasenlazadas;
+
+/**
+ *
+ * @author carlospalominovidal
+ */
+public class Nodo {
+    // Atributo para almacenar el valor del nodo (de tipo Producto)
+    private Producto valor;
+
+    // Referencia al siguiente nodo en la lista
+    private Nodo siguiente;
+
+    // Constructor para crear un nodo con un valor dado (de tipo Producto)
+    public Nodo(Producto valor) {
+        this.valor = valor;
+        this.siguiente = null; // Inicialmente, no hay siguiente nodo
+    }
+
+    // Métodos Getter y Setter para el valor del nodo
+    public Producto getValor() {
+        return valor;
+    }
+
+    public void setValor(Producto valor) {
+        this.valor = valor;
+    }
+
+    // Método Getter para el siguiente nodo
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    // Método Setter para establecer el siguiente nodo
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+}
---
+++ src/frmlistasenlazadas/testListaEnlazada.java
@@ -0,0 +1,29 @@
+
+package frmlistasenlazadas;
+
+public class testListaEnlazada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+                // Crear una lista enlazada vacía
+        ListaEnlazada lista = new ListaEnlazada();
+
+        // Crear algunos objetos Producto
+        Producto producto1 = new Producto(1, "Producto 1", "Categoría A");
+        Producto producto2 = new Producto(2, "Producto 2", "Categoría B");
+        Producto producto3 = new Producto(3, "Producto 3", "Categoría A");
+
+        // Agregar los productos a la lista
+        lista.agregarNodo(producto1);
+        lista.agregarNodo(producto2);
+        lista.agregarNodo(producto3);
+
+        // Imprimir los elementos de la lista
+        lista.imprimir();
+    }
+    
+}
